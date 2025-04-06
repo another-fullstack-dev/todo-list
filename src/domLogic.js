1c94e300d7ea73eac68366dc47fdbedff56730de
odinite№490028 <97753719+another-fullstack-dev@users.noreply.github.com>
2025-04-06 12:26:06 +0700

    add eslint

--- src/domLogic.js
+++ src/domLogic.js
@@ -1,128 +1,135 @@
-import { content, projectDiv, projectList, projectSelect, Todo, Project } from "./index";
+import {
+  content,
+  projectDiv,
+  projectList,
+  projectSelect,
+  Todo,
+  Project,
+} from "./index";
 
 function createTodo(object) {
-    let div = document.createElement('div');
-    let h2 = document.createElement('h2');
-    let p = document.createElement('p');
-    let footer = document.createElement('footer');
-    let dueSpan = document.createElement('span');
-    let prioSpan = document.createElement('span');
-    let completedBtn = document.createElement('button');
-    let removeBtn = document.createElement('button');
+  let div = document.createElement("div");
+  let h2 = document.createElement("h2");
+  let p = document.createElement("p");
+  let footer = document.createElement("footer");
+  let dueSpan = document.createElement("span");
+  let prioSpan = document.createElement("span");
+  let completedBtn = document.createElement("button");
+  let removeBtn = document.createElement("button");
 
-    footer.appendChild(dueSpan);
-    footer.appendChild(prioSpan);
-    footer.appendChild(completedBtn);
-    footer.appendChild(removeBtn);
-    
-    completedBtn.addEventListener('click', () => {
-        if (object.completed){
-            return;
-        };
-        object.completed = true;
-        div.style.opacity = 0.5;
-        localStorage.setItem(object.title, JSON.stringify(object));
-    })
-    
-    removeBtn.addEventListener('click', () => {
-        content.removeChild(div);
-        localStorage.removeItem(object.title);
-    })
+  footer.appendChild(dueSpan);
+  footer.appendChild(prioSpan);
+  footer.appendChild(completedBtn);
+  footer.appendChild(removeBtn);
 
-    div.appendChild(h2);
-    div.appendChild(p);
-    div.appendChild(footer);
-
-    h2.textContent = object.title;
-    p.textContent = object.description;
-    dueSpan.textContent = object.dueDate;
-    prioSpan.textContent = object.priority;
-    completedBtn.textContent = "Mark as completed"
-    removeBtn.textContent = "X";
-
-    if (object.completed){
-        div.style.opacity = 0.5;
+  completedBtn.addEventListener("click", () => {
+    if (object.completed) {
+      return;
     }
+    object.completed = true;
+    div.style.opacity = 0.5;
+    localStorage.setItem(object.title, JSON.stringify(object));
+  });
 
-    div.classList.add('todo')
+  removeBtn.addEventListener("click", () => {
+    content.removeChild(div);
+    localStorage.removeItem(object.title);
+  });
 
-    return div;
-};
+  div.appendChild(h2);
+  div.appendChild(p);
+  div.appendChild(footer);
 
-const dialogCreateTodo = document.querySelector('.dialog-todo');
-const dialogCreateProject = document.querySelector('.dialog-project');
-const addTodoBtn = document.querySelector('.add');
-const addProjectBtn = document.querySelector('.btn-add-project');
-const closeModal = document.querySelectorAll('.close');
-const inputsTodo = document.querySelectorAll('.form-todo > input');
-const inputsProject = document.querySelectorAll('.form-project > input');
-const inputs = document.querySelectorAll('form > input');
+  h2.textContent = object.title;
+  p.textContent = object.description;
+  dueSpan.textContent = object.dueDate;
+  prioSpan.textContent = object.priority;
+  completedBtn.textContent = "Mark as completed";
+  removeBtn.textContent = "X";
 
-const mainProject = document.querySelector('.main-page-li');
-mainProject.addEventListener('click', () => {
+  if (object.completed) {
+    div.style.opacity = 0.5;
+  }
+
+  div.classList.add("todo");
+
+  return div;
+}
+
+const dialogCreateTodo = document.querySelector(".dialog-todo");
+const dialogCreateProject = document.querySelector(".dialog-project");
+const addTodoBtn = document.querySelector(".add");
+const addProjectBtn = document.querySelector(".btn-add-project");
+const closeModal = document.querySelectorAll(".close");
+const inputsTodo = document.querySelectorAll(".form-todo > input");
+const inputsProject = document.querySelectorAll(".form-project > input");
+const inputs = document.querySelectorAll("form > input");
+
+const mainProject = document.querySelector(".main-page-li");
+mainProject.addEventListener("click", () => {
+  clearContent();
+});
+
+function createProject(object) {
+  let p = document.createElement("p");
+  let li = document.createElement("li");
+  li.appendChild(p);
+  p.textContent = object.title;
+  li.addEventListener("click", () => {
     clearContent();
-});
-
-function createProject(object){
-    let p = document.createElement('p');
-    let li = document.createElement('li');
-    li.appendChild(p);
-    p.textContent = object.title;
-    li.addEventListener('click', () => {
-        clearContent();
-        localStorage.setItem("project", p.textContent);
-        object.todos.forEach(todo => {
-            content.appendChild(createTodo(todo));
-        });
+    localStorage.setItem("project", p.textContent);
+    object.todos.forEach((todo) => {
+      content.appendChild(createTodo(todo));
     });
+  });
 
-    return li;
-};
+  return li;
+}
 
-addTodoBtn.addEventListener('click', () => {
-    dialogCreateTodo.showModal();
+addTodoBtn.addEventListener("click", () => {
+  dialogCreateTodo.showModal();
 });
 
-addProjectBtn.addEventListener('click', () => {
-    dialogCreateProject.showModal();
-})
+addProjectBtn.addEventListener("click", () => {
+  dialogCreateProject.showModal();
+});
 
 closeModal.forEach((button) => {
-    button.addEventListener('click', () => {
-        button.parentElement.parentElement.parentElement.close(); // ok
-        inputs.forEach((node)=>{
-        node.value = '';
+  button.addEventListener("click", () => {
+    button.parentElement.parentElement.parentElement.close(); // ok
+    inputs.forEach((node) => {
+      node.value = "";
     });
-    })
-})
-
-const projectForm = document.querySelector('.form-project');
-projectForm.addEventListener('submit', () => {
-        let array = [];
-        inputsProject.forEach((node)=>{
-            array.push(node.value);
-            node.value = '';
-        });
-        let project = new Project(array[0]);
-        localStorage.setItem(project.title, JSON.stringify(project));
+  });
 });
 
-const todoForm = document.querySelector('.form-todo');
-todoForm.addEventListener('submit', () => {
-        let array = [];
-        inputsTodo.forEach((node)=>{
-            array.push(node.value);
-            node.value = '';
-        });
-        let todo = new Todo(array[0],array[1],array[2],array[3]);
-        content.appendChild(createTodo(todo));
-        localStorage.setItem(todo.title, JSON.stringify(todo));
+const projectForm = document.querySelector(".form-project");
+projectForm.addEventListener("submit", () => {
+  let array = [];
+  inputsProject.forEach((node) => {
+    array.push(node.value);
+    node.value = "";
+  });
+  let project = new Project(array[0]);
+  localStorage.setItem(project.title, JSON.stringify(project));
 });
 
-function clearContent(){
-    while(content.lastChild){
-        content.removeChild(content.lastChild);
-    };
-};
+const todoForm = document.querySelector(".form-todo");
+todoForm.addEventListener("submit", () => {
+  let array = [];
+  inputsTodo.forEach((node) => {
+    array.push(node.value);
+    node.value = "";
+  });
+  let todo = new Todo(array[0], array[1], array[2], array[3]);
+  content.appendChild(createTodo(todo));
+  localStorage.setItem(todo.title, JSON.stringify(todo));
+});
+
+function clearContent() {
+  while (content.lastChild) {
+    content.removeChild(content.lastChild);
+  }
+}
 
 export { createTodo, createProject };
--- src/index.js
+++ src/index.js
@@ -1,36 +1,42 @@
 import "./styles.css";
 import { createTodo, createProject } from "./domLogic";
 
-const content = document.querySelector('#content');
-const projectDiv = document.querySelector('.project');
-const projectList = document.querySelector('.project-list');
-const projectSelect = document.querySelector('#project-select');
+const content = document.querySelector("#content");
+const projectDiv = document.querySelector(".project");
+const projectList = document.querySelector(".project-list");
+const projectSelect = document.querySelector("#project-select");
 
 class Todo {
-    constructor(title, description = '', dueDate = '', priority = '', type = "todo") {
-        this.title = title;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.priority = priority;
-        this.completed = false;
-        this.type = type;
-    };
-};
+  constructor(
+    title,
+    description = "",
+    dueDate = "",
+    priority = "",
+    type = "todo"
+  ) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.completed = false;
+    this.type = type;
+  }
+}
 
 class Project {
-    constructor(title) {
-        this.title = title;
-        this.todos = [];
-        this.type = "project";
-    };
-};
+  constructor(title) {
+    this.title = title;
+    this.todos = [];
+    this.type = "project";
+  }
+}
 
-for (let i = 0; i < localStorage.length; i++){
-    let item = JSON.parse((localStorage.getItem(localStorage.key(i))));
-    if (item.type == "project"){
-        continue
-    };
-    content.appendChild(createTodo(item));
-};
+for (let i = 0; i < localStorage.length; i++) {
+  let item = JSON.parse(localStorage.getItem(localStorage.key(i)));
+  if (item.type == "project") {
+    continue;
+  }
+  content.appendChild(createTodo(item));
+}
 
 export { content, projectDiv, projectList, projectSelect, Todo, Project };
